4a4aa0c105868561dfe4cf7f84b90f3949073130
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-11 02:18:57 +0900

    Object.values

--- ES7ES8.js
+++ ES7ES8.js
@@ -2,5 +2,14 @@
 
 const string = "12345";
 
-console.log(string.padStart(10, "."));
-console.log(string.padEnd(10, "."));
+// console.log(string.padStart(10, "."));
+// console.log(string.padEnd(10, "."));
+
+// Object.values
+
+const object = {
+  name: "John",
+  age: 20,
+};
+
+console.log(Object.values(object));
